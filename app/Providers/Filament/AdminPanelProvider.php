fd475cb2b948cda1e6d4fbbbaeb92b04d0f259cd
liuscifer <150527871+liuscifer@users.noreply.github.com>
2025-06-23 13:54:32 +0700

    foto filament udah tampil

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -17,6 +17,7 @@ use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
+use Filament\SpatieLaravelMediaLibraryPlugin\SpatieLaravelMediaLibraryPlugin;
 
 class AdminPanelProvider extends PanelProvider
 {
@@ -26,6 +27,14 @@ class AdminPanelProvider extends PanelProvider
             ->default()
             ->id('admin')
             ->path('admin')
+            // ->resources([
+            //     ProductResource::class,
+            //     ContactResource::class,
+            //     AttendanceResource::class,
+            //     ])
+            // ->plugins([
+            // SpatieLaravelMediaLibraryPlugin::make(),
+            //     ])
             ->login()
             ->colors([
                 'primary' => Color::Amber,
